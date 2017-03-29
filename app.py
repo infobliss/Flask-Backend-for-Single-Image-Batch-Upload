8a2dcfbe17f05c6281a9d4988175ef341029a635
infobliss <sid.nitdurgapur@gmail.com>
2017-03-29 06:57:17 +0530

    sibu

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify,render_template, request
+from listOfGlams import list_glams
+from NationaalArchief import main
+import urllib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Main page."""
+    return render_template('login.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def receiveData():
+    glam1 = request.form['glam_name']
+    id = request.form['file_id']
+    glam_list = list_glams()
+    IsValidGLAM = 0
+    for glam in glam_list: 
+         if glam['name'] == glam1:
+		IsValidGLAM=1
+    		break
+    if IsValidGLAM == 0:
+	 return "GLAM Not Found in our list"
+    else:
+         #upload the image
+         howManyMatches = main(id)
+	 print('passes main')
+	 if howManyMatches == 0:
+		return 'No match found!'
+	 else:
+                returnString = 'Upload of ' + str(howManyMatches) + ' files successful. or File already exists'
+	        return returnString
+
+@app.route('/glam', methods=['GET'])
+def showForm():
+    """Glam Form"""
+    return render_template('glam_form.html')	
+
+if __name__ == "__main__":
+    app.run()
---
+++ listOfGlams.py
@@ -0,0 +1,25 @@
+def list_glams():
+     return [{
+	'name' : 'Nationaal Archief'
+     },
+     {
+	'name' : 'NASA EOL'
+     },
+     {
+	'name' : 'AucklandMuseumCCBY'
+     },
+     {
+	'name' : 'Imperial Encyclopaedia'	
+     },
+     {
+	'name' : 'Catharijne Convent'
+     },
+     {
+	'name' : 'Public domain films'	
+     },
+     {
+	'name' : 'Tropenmuseum Expeditions'
+     },
+     {
+	'name' : 'Trainpix'
+     }]
